54ec416e6724e5a961e3e027924171bff9e3f4ab
Rion Dooley <deardooley@gmail.com>
2022-07-31 15:34:24 -0500

    Adding slack webhook notifications on error and success. Adding snapshot zip archiving after successful timelapse video creation. Formalizing timestamps to ISO8601 format

--- src/timelapse.ts
+++ src/timelapse.ts
@@ -1,12 +1,20 @@
 // Copyright (c) Wictor Wilén. All rights reserved. 
 // Licensed under the MIT license.
 
-import { writeFileSync, rmSync, existsSync, readdirSync, lstatSync } from 'fs';
+import {writeFileSync, rmSync, readdirSync, createWriteStream, readFileSync, lstatSync, statSync} from 'fs';
 import * as path from 'path'
 import FfmpegCommand from 'fluent-ffmpeg';
+const { IncomingWebhook } = require('@slack/webhook');
+var JSZip = require("jszip");
+
+const url = process.env.SLACK_WEBHOOK_URL;
+// Initialize with defaults
+const webhook = new IncomingWebhook(url, {
+    icon_emoji: ':camera:',
+});
 
 async function timelapse() {
-
+    const tlts = new Date().toISOString().replace(':','_',).replace(':','_',);
     const folders = readdirSync(path.resolve(__dirname, "target"));
     folders.forEach(f => {
         if (lstatSync(path.resolve(__dirname, "target", f)).isDirectory()) {
@@ -14,23 +22,55 @@ async function timelapse() {
 
             let command = FfmpegCommand();
 
-            const files = readdirSync(path.resolve(__dirname, "target", f));
             let template = "";
-            const templateFilePath = path.resolve(__dirname, "target", f + "-" + Date.now() + '.txt');
+            const files = readdirSync(path.resolve(__dirname, "target", f));
+            const templateFilePath = path.resolve(__dirname, "target", f + "-" + tlts + '.txt');
+            const timelapseFilePath = path.resolve(__dirname, "target", f + "-" + tlts + '.mp4');
+            const archiveFilePath = path.resolve(__dirname, "target", f + "-" + tlts + '.zip');
 
 
             command.on('error', (err) => {
                 console.log('An error occurred: ' + err.message);
+                slackError(`Failed creating ${f} camera timelapse`);
             });
 
             // Cleanup commands once timelapse is done
             command.on('end', () => {
-                console.log('Merging finished, removing snapshot images!');
-                for (const file of files) {
-                    rmSync(path.resolve(__dirname, "target", f, file));
-                }
+                console.log(`Done merging ${f} camera snapshots. Creating daily archive.`);
+
+                // clea up the ffmpeg command file
                 rmSync(templateFilePath);
-                console.log("Done!");
+
+                // create zip archive for the timelapsed images we just calculated
+                var zip = new JSZip();
+                for (const file of files) {
+                    // rmSync(path.resolve(__dirname, "target", f, file));
+                    const snapshotFilePath = path.resolve(__dirname, "target", f, file);
+                    const snapshotStat = statSync(snapshotFilePath);
+
+                    console.log(`Timestamp of file ${snapshotFilePath} is ` + snapshotStat.ctime.toISOString());
+                    console.log(`Timestamp of file ${snapshotFilePath} is ` + snapshotStat.ctimeMs);
+                    console.log(snapshotStat);
+                    // add file to zip archive
+                    zip.file(file, readFileSync(snapshotFilePath), {
+                        date: snapshotStat.ctime
+                    });
+                }
+
+                zip.generateNodeStream({type:'nodebuffer',streamFiles:true})
+                   .pipe(createWriteStream(archiveFilePath))
+                   .on('finish', function () {
+                       console.log(`Done archiving ${f} camera snapshots!`);
+                       slackError(`New timelapse available for camera ${f}`);
+                       // remove the files for the next day's snapshots
+                       for (const file of files) {
+                           rmSync(path.resolve(__dirname, "target", f, file));
+                       }
+                   })
+                   .on('error', function() {
+                       console.log(`Error archiving ${f} camera snapshots`);
+                       slackError(`Error archiving ${f} camera shapshots`);
+                   });
             });
 
             // add all the image files
@@ -57,10 +97,17 @@ async function timelapse() {
             command.format("mp4");
 
             // persist the file
-            command.save(path.resolve(__dirname, "target", f + "-" + Date.now() + '.mp4'));
+            command.save(timelapseFilePath);
         }
     });
 
+}
+
+// Send the notification
+async function slackError(msg: String) {
+    await webhook.send({
+        text: msg,
+    });
 }
 
 timelapse();
