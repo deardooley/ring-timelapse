64e6ef50d40b77bb63727e7a4faddee5f8f689d5
Rion Dooley <deardooley@gmail.com>
2022-07-31 15:36:12 -0500

    Moved the snapshot and timelapse cron schedule definition to the run.sh script so it can be overridden at startup.

--- cron/run.sh
+++ cron/run.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+# Setup the cron job to
+echo "$CRON_SCHEDULE cd /app && date && timeout 30s npm run snapshot" >> /etc/crontabs/root
+echo "$CRON_SCHEDULE_TIMELAPSE cd /app && date && timeout 120s npm run timelapse" >> /etc/crontabs/root
+
 crond -L /var/log/cron.log && tail -f /var/log/cron.log
