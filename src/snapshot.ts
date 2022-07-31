1a541916f18c2c00b32170f6ae492a42bc259c9f
Rion Dooley <deardooley@gmail.com>
2022-07-31 15:33:10 -0500

    Adding slack webhook notifications on error and success.

--- src/snapshot.ts
+++ src/snapshot.ts
@@ -6,8 +6,15 @@ import { RingApi } from 'ring-client-api'
 import * as path from 'path'
 import * as dotenv from "dotenv";
 import * as lodash from "lodash";
+const { IncomingWebhook } = require('@slack/webhook');
 
 const log = console.log;
+
+const url = process.env.SLACK_WEBHOOK_URL;
+// Initialize with defaults
+const webhook = new IncomingWebhook(url, {
+    icon_emoji: ':camera:',
+});
 
 async function snapshot() {
     log("running snapshot")
@@ -25,25 +32,39 @@ async function snapshot() {
 
     cameras.forEach(camera => {
         const name = lodash.camelCase(camera.name);
-        log(`Retrieving snapshot for ${camera.name}`);
+        if (name !== 'frontDoor') {
+            log(`Retrieving snapshot for ${camera.name}`);
 
-        camera.getSnapshot().then(function (result) {
-            try {
-                log((path.resolve(__dirname, "target", name)));
-                if (!existsSync(path.resolve(__dirname, "target", name))) {
-                    mkdirSync(path.resolve(__dirname, "target", name));
+            camera.getSnapshot().then(function (result) {
+                try {
+                    log((path.resolve(__dirname, "target", name)));
+                    if (!existsSync(path.resolve(__dirname, "target", name))) {
+                        mkdirSync(path.resolve(__dirname, "target", name));
+                    }
+                    writeFile(path.resolve(__dirname, "target", path.join(name, Date.now() + '.png')), result, (err) => {
+                        if (err) throw err;
+                        log('Saved!');
+                    });
+                } catch (err) {
+                    log(`Error: ${err}`);
+                    slackError(`Failed saving snapshot from camera ${camera.name}: ${err.message}`);
                 }
-                writeFile(path.resolve(__dirname, "target", path.join(name, Date.now() + '.png')), result, (err) => {
-                    if (err) throw err;
-                    log('Saved!');
-                });
-            } catch (err) {
-                log(`Error: ${err}`);
-            }
 
-        }).catch(err => {
-            log(`Snapshot error: ${err}`);
-        })
+            }).catch(err => {
+                log(`Snapshot error: ${err}`);
+                slackError(`Failed fetching snapshot from camera ${camera.name}: ${err.message}`);
+                slackError(err);
+            })
+        } else {
+            log(`Skipping snapshot for ${camera.name}`);
+        }
+    });
+}
+
+// Send the notification
+async function slackError(msg: String) {
+    await webhook.send({
+        text: msg,
     });
 }
 
